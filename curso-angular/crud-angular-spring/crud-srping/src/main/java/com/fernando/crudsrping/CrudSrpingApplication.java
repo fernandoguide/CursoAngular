774ce0bb6d264cd3a2ef50fd7e62921f6525e36f
Fernando <fernando-guide@hotmail.com>
2021-11-28 10:41:41 -0300

    add crud-spring

---
+++ curso-angular/crud-angular-spring/crud-srping/src/main/java/com/fernando/crudsrping/CrudSrpingApplication.java
@@ -0,0 +1,13 @@
+package com.fernando.crudsrping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSrpingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSrpingApplication.class, args);
+	}
+
+}
---
+++ curso-angular/crud-angular-spring/crud-srping/src/test/java/com/fernando/crudsrping/CrudSrpingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernando.crudsrping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSrpingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
